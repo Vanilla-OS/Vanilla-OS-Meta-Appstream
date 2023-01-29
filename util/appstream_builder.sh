b295095c1336b3070cc8e9e10ac745b8d57e3344
Mateus Melchiades <mateusbmelchiades@gmail.com>
2023-01-29 19:10:36 -0300

    Collection creator util + testing applications

---
+++ util/appstream_builder.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+SEPARATOR="-------------------------------------------------------------------------------"
+
+download_icon() {
+    local APPID=$1
+    local ICON_URL=$(sed -rn 's/.*<icon type="remote">(.*)<\/icon>/\1/p' packages/usr/share/metainfo/$APPID.metainfo.xml)
+
+    echo "Downloading icon for $APPID..."
+    wget -O packages/usr/share/icons/hicolor/scalable/apps/$APPID.svg $ICON_URL 2> /dev/null
+}
+
+get_app_ids() {
+    ls -1 packages/usr/share/metainfo/ | rev | cut -d '.' -f 3- | rev
+}
+
+escape_string() {
+    echo -n $1 | sed -r -n 's/([\.\/\"])/\\\1/gp'
+}
+
+replace_icon_ids() {
+    local APPID=$1
+    local LINE=$(cat packages/usr/share/metainfo/$APPID.metainfo.xml | grep "<icon type=\"remote\".*")
+    local ESC_LINE=$(escape_string "$LINE")
+    local ESC_APPID=$(escape_string "$APPID")
+    sed -r -z "s/([ \t]*<icon[^\n]*>$ESC_APPID[a-z\.]*<\/icon>\n){1,}/    $ESC_LINE\n/" -i output/vanilla_meta.xml
+}
+
+
+# Download icons required by appstreamcli compose
+echo "Downloading remote icons...\n"
+echo $(get_app_ids) | tr ' ' '\n' | while read entry; do
+    download_icon $entry
+done
+
+# Generate Appstream
+echo $SEPARATOR
+echo "Generating Appstream...\n"
+appstreamcli compose packages --data-dir=output --hints-dir=output/hints --origin="vanilla_meta"
+echo "Appstream generated at output/vanilla_meta.xml.gz"
+
+# Validate
+if [ "$1" != "--novalidate" ]; then
+    echo $SEPARATOR
+    echo "Validating Appstream...\n"
+    appstreamcli validate output/vanilla_meta.xml.gz
+fi
+
+# Replace fixed icons with remote, as gnome-software caches icons for us
+echo $SEPARATOR
+echo "Replacing local icons with remote name...\n"
+gzip -d output/vanilla_meta.xml.gz
+echo $(get_app_ids) | tr ' ' '\n' | while read entry; do
+    replace_icon_ids $entry
+done
+gzip output/vanilla_meta.xml
+
+# Cleanup
+echo $SEPARATOR
+echo "Running cleanup..."
+rm -rf packages/usr/share/swcatalog
+rm -rf packages/usr/share/icons/hicolor/scalable/apps/*
+
+echo $SEPARATOR
+echo "Complete!"
